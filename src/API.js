c5a5f4e9408404440b075587647ecb9a80c64270
Aaron <aaronli5260@gmail.com>
2022-09-18 15:33:28 -0500

    created routes for backend, mock song

--- src/API.js
+++ src/API.js
@@ -1,8 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import songsRoutes from "./routes/songs.js"
 
 const app = express();
 const PORT = 5001;
 
 app.use(bodyParser.json());
-app.listen(PORT, () => console.log('Server running'))
+
+app.use('/songs', songsRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello, homepage')
+});
+
+app.listen(PORT, () => console.log('Server running on port 5001'));
