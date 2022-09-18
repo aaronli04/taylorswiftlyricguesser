c0d05a5e837437f936f4ef1a117ae5a1e7d57664
Aaron <aaronli5260@gmail.com>
2022-09-18 15:08:05 -0500

    starting to build API, console logging with node.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Swiftinator
-    </div>
+    <>
+      <div className='App'>
+        <div className='Title'>
+          Swiftinator
+        </div>
+      </div>
+    </>
   );
 }
 
