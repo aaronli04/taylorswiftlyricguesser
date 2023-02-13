d616ea7b5481dd2e520522ecf4ed7eb0ac532899
Aaron <aaronli5260@gmail.com>
2023-02-13 11:56:21 -0600

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './frontend/App';
-
+//test
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
